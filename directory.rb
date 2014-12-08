6ad70662ce21024ddb37444d8f263170b30cf13a
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-08 15:18:21 +0000

    print if student == a

--- directory.rb
+++ directory.rb
@@ -5,7 +5,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
+    if student[:name] =~ /A/
     puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+  end
   end
 end
 
