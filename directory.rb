846e8c840eced9a9b5f7f00df1c4c140881c06cf
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-08 22:21:33 +0000

    Adding .gitignore File

--- directory.rb
+++ directory.rb
@@ -55,11 +55,24 @@ def process(selection)
   end
 end
 
+
 def interactive_menu  
   loop do
     print_menu        
     process(gets.chomp)
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
